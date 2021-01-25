45e9f9b478796d5cea07e5292254a0fd6456fd7e
Valeria-CPaz <21214@student.dorset-college.ie>
2021-01-25 23:31:59 +0000

    commit

---
+++ script.js
@@ -0,0 +1,28 @@
+var myAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+
+function myAlphabetLength() {
+    console.log(myAlphabet.length);
+}
+myAlphabetLength();
+
+let planets = ['Earth', 'Mars', 'Mercury', 'Saturn', 'Jupter'];
+console.log(planets[0]);
+console.log(planets[1]);
+console.log(planets[2]);
+console.log(planets[3]);
+console.log(planets[4]);
+
+let wowDataTypes = [1, 'Val', true];
+console.log(wowDataTypes[0]);
+console.log(wowDataTypes[1]);
+console.log(wowDataTypes[2]);
+
+// let hello = () => console.log('Hello world!');
+// hello();
+
+let hello = function() {
+    return 'Hello world!';
+}
+console.log(hello());
+
+
